1ad71bd9f07f42ad5508292baf388d1ab1d17f3d
Daniel Guerra <38929323+danguerra8282@users.noreply.github.com>
2023-08-30 15:22:59 -0400

    Create variables.tf

---
+++ terraform/codebuild/variables.tf
@@ -0,0 +1,19 @@
+variable account_id {
+  type        = string
+}
+
+variable codebuild_project_name {
+  type        = string
+}
+
+variable org_id {
+  type        = string
+}
+
+variable vpc_id {
+  type = string
+}
+
+variable subnets {
+  type = list
+}
