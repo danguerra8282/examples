ead4c7587ea0f2618dbbeccf818cbe07d0113a4b
Daniel Guerra <38929323+danguerra8282@users.noreply.github.com>
2023-09-19 15:41:09 -0400

    Update app.py

--- containers/flask/app.py
+++ containers/flask/app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def main():
-    return '<h1>This is the beginning</h1>'
+def home():
+    # return '<h1>This is the beginning</h1>'
+    return render_template('home.html')
+
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == "__main__":
