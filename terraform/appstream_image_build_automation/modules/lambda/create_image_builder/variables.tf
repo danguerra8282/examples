ac7dea0636548f19a66ddb5c604f62885c9c5a2a
Daniel Guerra <38929323+danguerra8282@users.noreply.github.com>
2024-03-06 15:29:46 -0500

    Add files via upload

---
+++ terraform/appstream_image_build_automation/modules/lambda/create_image_builder/variables.tf
@@ -0,0 +1,27 @@
+variable account_id {
+    type = string
+}
+
+variable appstream_image_builder_role_arn {
+    type = string
+}
+
+variable vpc_id {
+    type = string
+}
+
+# variable vpc_cidr {
+#     type = string
+# }
+
+variable private_subnet_ids {
+    type = list
+}
+
+variable appstream_base_image_name {
+    type = string
+}
+
+variable appstream_software_bucket_arn {
+    type = string
+}
